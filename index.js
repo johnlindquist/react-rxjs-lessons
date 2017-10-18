ad70b0e6d30984c7d21554396af70c4ad333fe11
John Lindquist <johnlindquist@computer.local>
2017-10-18 13:58:05 -0600

    counter

--- index.js
+++ index.js
@@ -9,34 +9,41 @@ import {
 
 setObservableConfig(config)
 
-const Counter = componentFromStream(props$ => {
-  const {
-    handler: inc,
-    stream: inc$
-  } = createEventHandler()
-  const {
-    handler: dec,
-    stream: dec$
-  } = createEventHandler()
+const Counter = ({ value, onInc, onDec }) => (
+  <div>
+    <button onClick={onInc}>+</button>
+    <h2>{value}</h2>
+    <button onClick={onDec}>-</button>
+  </div>
+)
 
-  return Observable.merge(
-    inc$.mapTo(1),
-    dec$.mapTo(-1)
-  )
-    .startWith(3)
-    .scan((acc, curr) => acc + curr)
-    .map(value => (
-      <div>
-        <button onClick={inc}>+</button>
-        <h2>{value}</h2>
-        <button onClick={dec}>-</button>
-      </div>
-    ))
-})
+const CounterStream = componentFromStream(
+  props$ => {
+    const {
+      handler: onInc,
+      stream: onInc$
+    } = createEventHandler()
+    const {
+      handler: onDec,
+      stream: onDec$
+    } = createEventHandler()
+
+    return props$.switchMap(props =>
+      Observable.merge(
+        onInc$.mapTo(1),
+        onDec$.mapTo(-1)
+      )
+        .startWith(props.value)
+        .scan((acc, curr) => acc + curr)
+        .map(value => ({ value, onInc, onDec }))
+        .map(Counter)
+    )
+  }
+)
 
 const App = () => (
   <div>
-    <Counter />
+    <CounterStream value={3} />
   </div>
 )
 
