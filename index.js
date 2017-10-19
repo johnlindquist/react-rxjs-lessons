863dbfded1658d7917b43933aec4112d566921f6
John Lindquist <johnlindquist@computer.local>
2017-10-18 19:32:31 -0600

    compose

--- index.js
+++ index.js
@@ -6,25 +6,87 @@ import {
   setObservableConfig,
   componentFromStream,
   createEventHandler,
-  mapPropsStream
+  mapPropsStream,
+  compose
 } from "recompose"
 
 setObservableConfig(config)
 //#endregion
 
-const interval = mapPropsStream(props$ =>
+const count = mapPropsStream(props$ => {
+  const {
+    stream: onInc$,
+    handler: onInc
+  } = createEventHandler()
+  const {
+    stream: onDec$,
+    handler: onDec
+  } = createEventHandler()
+
+  return props$.switchMap(
+    props =>
+      Observable.merge(
+        onInc$.mapTo(1),
+        onDec$.mapTo(-1)
+      )
+        .startWith(0)
+        .scan((acc, curr) => acc + curr),
+    (props, count) => ({
+      ...props,
+      count,
+      onInc,
+      onDec
+    })
+  )
+})
+const load = mapPropsStream(props$ =>
   props$.switchMap(
-    props => Observable.interval(1000),
-    (props, count) => ({ ...props, count })
+    props =>
+      Observable.ajax(
+        `https://swapi.co/api/people/${props.count}`
+      )
+        .pluck("response")
+        .startWith({ name: "loading..." })
+        .catch(err =>
+          Observable.of({ name: "Not found" })
+        ),
+    (props, person) => ({ ...props, person })
   )
 )
-const Counter = props => <h1>{props.count}</h1>
 
-const CounterWithInterval = interval(Counter)
+const typewriter = mapPropsStream(props$ =>
+  props$.switchMap(
+    props =>
+      Observable.zip(
+        Observable.from(props.person.name),
+        Observable.interval(100),
+        letter => letter
+      ).scan((acc, curr) => acc + curr),
+    (props, name) => ({
+      ...props,
+      person: { ...props.person, name }
+    })
+  )
+)
+
+const Counter = props => (
+  <div>
+    <button onClick={props.onInc}>+</button>
+    <button onClick={props.onDec}>-</button>
+    <h3>{props.count}</h3>
+    <h1>{props.person.name}</h1>
+  </div>
+)
+
+const CounterWithPersonLoader = compose(
+  count,
+  load,
+  typewriter
+)(Counter)
 
 const App = () => (
   <div>
-    <CounterWithInterval />
+    <CounterWithPersonLoader />
   </div>
 )
 
